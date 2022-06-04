1002efbda2db6613ef2e86a312e7c88b660f391b
ArnoldSGR <arnoldrodrigues11@gmail.com>
2022-06-04 20:16:54 -0300

    Co-authored-by: S  <sviegasdc@users.noreply.github.com>

---
+++ src/lib/GameCard.svelte
@@ -0,0 +1,47 @@
+<script>
+	import '/src/app.css';
+</script>
+
+<div class="main-container">
+	<div class="card-header">Aqui o header</div>
+	<div class="card-content">Aqui o conteúdo</div>
+	<div class="card-footer">Aqui o footer</div>
+</div>
+
+<style>
+	.main-container {
+		width: 300px;
+		height: 300px;
+		border-radius: 27px;
+		background-color: rgba(250, 250, 250, 0.3);
+		display: flex;
+		flex-direction: column;
+		justify-content: space-evenly;
+		align-items: center;
+		margin: 0 10px;
+	}
+	.card-header {
+		display: flex;
+		flex-direction: row;
+		border: 1px solid red;
+		height: 33.33%;
+		width: 92%;
+        padding: 10px;
+	}
+	.card-content {
+		display: flex;
+		flex-direction: row;
+		border: 1px solid red;
+		height: 33.33%;
+		width: 100%;
+        padding: 10px 0 ;
+	}
+	.card-footer {
+		display: flex;
+		flex-direction: row;
+		border: 1px solid red;
+		height: 33.33%;
+		width: 92%;
+        padding: 10px;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import GameCard from '$lib/GameCard.svelte';
+	import '/src/app.css';
+	let jogos = ['', '', ''];
+</script>
+
+<div class="main">
+	<div class="logo">
+		<img
+			src="https://www.qatar2022.qa/themes/custom/sc/dist/img/svg/qatar-hero-shadow.svg"
+			alt="Logo do Qatar"
+            width="250px"
+            height="250px"
+		/>
+	</div>
+    <div class="links">
+        <a href="/">Fase de Grupos</a>
+        <a href="/">Fase Final</a>
+    </div>
+	<div class="show-games">
+		{#each jogos as jogo}
+			<GameCard />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.main {
+		height: 100vh;
+		width: 100vw;
+		background-image: url(https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/background.png);
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center center;
+		background-attachment: fixed;
+	}
+	.show-games {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: center;
+		align-items: center;
+		overflow: auto;
+	}
+	.logo {
+		height: 150px;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: center;
+		align-items: center;
+		/* border: 1px solid red; */
+	}
+    .links a{
+        color:white;
+        text-decoration: none;
+        font-size: 50px;
+        display:flexbox;
+    }
+</style>
