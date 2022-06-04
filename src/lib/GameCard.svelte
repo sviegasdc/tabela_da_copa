b46a985c2f25659363e82eb266f7282af41d700c
ArnoldSGR <arnoldrodrigues11@gmail.com>
2022-06-04 20:40:27 -0300

    Update GameCard.svelte

--- src/lib/GameCard.svelte
+++ src/lib/GameCard.svelte
@@ -14,16 +14,17 @@
 		height: 300px;
 		border-radius: 27px;
 		background-color: rgba(250, 250, 250, 0.3);
+        color: #fafafa;
 		display: flex;
 		flex-direction: column;
 		justify-content: space-evenly;
 		align-items: center;
 		margin: 0 10px;
+        overflow: auto;
 	}
 	.card-header {
 		display: flex;
 		flex-direction: row;
-		border: 1px solid red;
 		height: 33.33%;
 		width: 92%;
         padding: 10px;
@@ -31,17 +32,18 @@
 	.card-content {
 		display: flex;
 		flex-direction: row;
-		border: 1px solid red;
 		height: 33.33%;
-		width: 100%;
+		width: 99%;
         padding: 10px 0 ;
 	}
 	.card-footer {
 		display: flex;
 		flex-direction: row;
-		border: 1px solid red;
 		height: 33.33%;
 		width: 92%;
         padding: 10px;
 	}
+    @media (max-width: 768px){
+        
+    }
 </style>
