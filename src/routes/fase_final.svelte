a0bb3e09a6c094bdf068b60c328410ab4fd92298
ArnoldSGR <arnoldrodrigues11@gmail.com>
2022-06-13 22:38:02 -0300

    Create fase_final.svelte

---
+++ src/routes/fase_final.svelte
@@ -0,0 +1,34 @@
+<script>
+    import '/src/app.css';
+</script>
+<div class="main">
+	<div class="logo">
+		<img
+			src="https://www.qatar2022.qa/themes/custom/sc/dist/img/svg/qatar-hero-shadow.svg"
+			alt="Logo do Qatar"
+			width="250px"
+			height="250px"
+		/>
+	</div>
+</div>
+
+<style>
+	.main {
+		height: 100vh;
+		width: 100vw;
+		background-image: url(https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/background.png);
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center center;
+		background-attachment: fixed;
+	}
+    .logo {
+		height: 100px;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: center;
+		align-items: center;
+		/* border: 1px solid red; */
+	}
+</style>
