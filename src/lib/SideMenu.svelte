b29076dc6fd633fc6ab30fce56a5ea493e64059e
ArnoldSGR <arnoldrodrigues11@gmail.com>
2022-07-06 22:47:53 -0300

    Update SideMenu.svelte

--- src/lib/SideMenu.svelte
+++ src/lib/SideMenu.svelte
@@ -1,21 +1,24 @@
 <script>
 	import '/src/app.css';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
 	let isOpen = false;
 	let displayStadium = false;
 	let displaySelecao = false;
 	let displayByDate = false;
 	let estadios = [
-		{ name: 'Lusail Stadium' }, 
-		{ name: 'Khalifa International Stadium' }, 
+		{ name: 'Lusail Stadium' },
+		{ name: 'Khalifa International Stadium' },
 		{ name: 'Al Thumama Stadium' },
-		{ name: 'Al Janoub Stadium' }, 
-		{ name: 'Al Bayt Stadium' }, 
-		{ name: 'Ahmad Bin Ali Stadium' }, 
-		{ name: 'Education City Stadium' }, 
+		{ name: 'Al Janoub Stadium' },
+		{ name: 'All Bayt Stadium' },
+		{ name: 'Ahmad Bin Ali Stadium' },
+		{ name: 'Education City Stadium' },
 		{ name: 'Stadium 974' }
 	];
 	let selecoes = [
-		{ name: 'Catar' },
+		{ name: 'Qatar' },
 		{ name: 'Equador' },
 		{ name: 'Senegal' },
 		{ name: 'Holanda' },
@@ -60,15 +63,34 @@
 		{ name: '01/12/2022' },
 		{ name: '02/12/2022' }
 	];
+	let selectedStadium = '';
+	let selectedTeam = '';
+	let selectedData = '';
+
+	const hdlStadiumClick = () => {
+		dispatch('stadiumSelect', selectedStadium);
+	};
+	const hdlTeamClick = () => {
+		dispatch('teamSelect', selectedTeam);
+	};
+	const hdlDataClick = () => {
+		console.log('escolheram essa data', selectedData);
+		dispatch('dataSelect', selectedData);
+	};
+	$: {
+		selectedStadium;
+		selectedTeam;
+		selectedData;
+	}
 </script>
 
 <button on:click={() => (isOpen = !isOpen)} class="control-button">
 	<img
-		src="https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/hamburguer.svg"
-		alt="hamburguer"
+		src="https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/filter.svg"
+		alt="filtrar jogos"
 		width="30px"
 		height="30px"
-	/>
+	/> Filtrar jogos
 </button>
 {#if isOpen}
 	<div class="sideMenu">
@@ -84,8 +106,8 @@
 				height="30px"
 			/>
 		</button>
-		<!-- lista de links massa -->
-		<button on:click={() => (displayStadium = !displayStadium)} class="dropDownOppenner">
+
+		<!-- <button on:click={() => (displayStadium = !displayStadium)} class="dropDownOppenner">
 			Estadios
 			<img
 				src="https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/triangulo.svg"
@@ -95,14 +117,19 @@
 				style="padding-left: 40px;"
 			/>
 		</button>
-		{#if displayStadium}
-			<div style="width:100%;overflow: auto; overflow-X: hidden;">
+		{#if displayStadium} -->
+		<h4 class="filtrar">Filtragem por estádio</h4>
+			<select class="select"
+				bind:value={selectedStadium}
+				on:change={() => hdlStadiumClick()}
+				style="width:100%;overflow: auto; overflow-X: hidden;"
+			>
 				{#each estadios as estadio}
-					<div class="card">{estadio.name}</div>
+					<option class="card">{estadio.name}</option>
 				{/each}
-			</div>
-		{/if}
-		<button on:click={() => (displaySelecao = !displaySelecao)} class="dropDownOppenner">
+			</select>
+		<!-- {/if} -->
+		<!-- <button on:click={() => (displaySelecao = !displaySelecao)} class="dropDownOppenner">
 			Seleções
 			<img
 				src="https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/triangulo.svg"
@@ -112,14 +139,19 @@
 				style="padding-left: 40px;"
 			/>
 		</button>
-		{#if displaySelecao}
-			<div style="width: 100%;overflow: auto;">
+		{#if displaySelecao} -->
+		<h4 class="filtrar2">Filtragem por equipe</h4>
+			<select class="select"
+				bind:value={selectedTeam}
+				on:change={() => hdlTeamClick()}
+				style="width: 100%;overflow: auto;"
+			>
 				{#each selecoes as selecao}
-					<div class="card">{selecao.name}</div>
+					<option class="card">{selecao.name}</option>
 				{/each}
-			</div>
-		{/if}
-		<button on:click={() => (displayByDate = !displayByDate)} class="dropDownOppenner">
+			</select>
+		<!-- {/if} -->
+		<!-- <button on:click={() => (displayByDate = !displayByDate)} class="dropDownOppenner">
 			Jogos por Dia
 			<img
 				src="https://raw.githubusercontent.com/ArnoldSGR/icons_tabela_da_copa/main/triangulo.svg"
@@ -129,17 +161,26 @@
 				style="padding-left: 15px;"
 			/>
 		</button>
-		{#if displayByDate}
-			<div style="width: 100%;overflow: auto; padding-bottom: 100px;">
+		{#if displayByDate} -->
+		<h4 class="filtrar3">Filtragem por data</h4>
+			<select class="select"
+				bind:value={selectedData}
+				on:change={() => hdlDataClick()}
+				style="width: 100%;overflow: auto;"
+			>
 				{#each datas as data}
-					<div class="card">{data.name}</div>
+					<option class="card">{data.name}</option>
 				{/each}
-			</div>
-		{/if}
+			</select>
+		<!-- {/if} -->
 	</div>
 {/if}
 
 <style>
+	h4 {
+		color: white;
+	}
+
 	.card {
 		color: white;
 		/* border: 1px solid red; */
@@ -152,15 +193,37 @@
 		overflow: auto;
 		padding: 4px;
 	}
-	.dropDownOppenner {
+	.select {
+		color: white;
+		font-weight: bold;
+		font-size: 18px;
+		background-color: #611f38;
+		padding-bottom: 2px;
+		outline: none;
+	}
+	
+	.filtrar2 {
+		padding-top: 20px;
+	}
+
+	.filtrar3 {
+		padding-top: 20px;
+	}
+
+	h4 {
+		font-size: 20px;
+	}
+ 
+	/* .dropDownOppenner {
 		background-color: cadetblue;
 		width: 100%;
 		min-height: fit-content;
 		height: 30px;
 		color: white;
 		font-size: 20px;
-	}
+	} */
 	.control-button {
+		color: white;
 		width: 40px;
 		height: 40px;
 		border: none;
