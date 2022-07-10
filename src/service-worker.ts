e32152b42a4bd5d20eb6d6ab2c53296edf12cb55
ArnoldSGR <arnoldrodrigues11@gmail.com>
2022-07-10 20:01:12 -0300

    pwa

---
+++ src/service-worker.ts
@@ -0,0 +1,47 @@
+import { build, files, version } from '$service-worker';
+import { precacheAndRoute, precache } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+declare let self: ServiceWorkerGlobalScope
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+
+precacheAndRoute([
+    ...build.map(f => {
+        return {
+            url: f,
+            revision: null
+        }
+    }),
+    ...files.map(f => {
+        return {
+            url: f,
+            revision: `${version}`
+        }
+    })
+]);
+
+// Edit the list of routes so they get cached and routed correctly, allowing
+// cold start or hot reload to work offline.
+const skRoutes = ['/', '/fasefinal', '/fasedegrupos'];
+
+precache(skRoutes.map(f => {
+    return {
+        url: f,
+        revision: `${version}`
+    }
+}));
+
+const matchCb = ({ url, request, event }) => {
+    return skRoutes.some(path => url.pathname === path);
+};
+registerRoute(matchCb, new StaleWhileRevalidate({}));
+
+
+
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
