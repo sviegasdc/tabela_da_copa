48a2528fac160ece88d3711776e35ae3512deb5d
ArnoldSGR <arnoldrodrigues11@gmail.com>
2022-08-18 22:22:27 -0300

    add playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,36 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 3000
-	}
-};
+// playwright.config.ts
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
 export default config;
+
+
+// /** @type {import('@playwright/test').PlaywrightTestConfig} */
+// const config = {
+// 	webServer: {
+// 		command: 'npm run build && npm run preview',
+// 		port: 3000
+// 	}
+// };
+
+// export default config;
